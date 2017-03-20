b43896917e5fa759ee53bea5379e21b111cb4315
kromkata <kk.manoilov@gmail.com>
2017-03-20 22:44:01 +0200

    play sound on load

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,13 +1,18 @@
 var soundID = "nosecurity";
+createjs.Sound.alternateExtensions = ["mp3"];
+createjs.Sound.on("fileload", this.loadHandler, this);
+createjs.Sound.registerSound("assets/nosecurity.mp3", soundID);
+ 
 
-function loadSound () {
-  createjs.Sound.registerSound("assets/nosecurity.mp3", soundID);
-}
+ function loadHandler(event) {
+     // This is fired for each sound that is registered.
+     var instance = createjs.Sound.play(soundID);  // play using id.  Could also use full sourcepath or event.src.
+     instance.on("complete", this.handleComplete, this);
+     instance.volume = 0.5;
 
-function playSound (soundfile, callback) {
-   createjs.Sound.play(soundID);
-}
 
-// #on.click.function(event){
-   
-// }
+ }
+
+
+
+
