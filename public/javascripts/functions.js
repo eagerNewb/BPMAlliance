a0d7aea03c4655ea87b4254fbd91b66642455aff
eagerNewb <kk.manoilov@gmail.com>
2017-03-21 16:16:54 +0200

    audio visualization, from microphone

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -17,17 +17,52 @@ function playPause(){
 }
 
 function volumeUpDown(volume){
-    var instanceVolume = volume * 1/100;
+  var instanceVolume = volume * 1/100;
 	instance.volume = instanceVolume;	
 	console.log(instanceVolume)	 
 
 }
+var audio = document.getElementById('my-audio');
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+var ctx = document.getElementById('canvas-ctx');
+var canvasCtx = ctx.getContext('2d');
+
+navigator.mediaDevices.getUserMedia({ audio: true, video:false}).then(function(stream) {
+   var source = audioCtx.createMediaStreamSource(stream);
+   var analyser = audioCtx.createAnalyser();
+   source.connect(analyser);
+
+  analyser.fftSize = 256;
+
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+
+  analyser.getByteTimeDomainData(dataArray);
+  canvasCtx.clearRect(0,0, canvasCtx.width, canvasCtx.height);
+  function draw() {
+
+      drawVisual = requestAnimationFrame(draw);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+      canvasCtx.fillRect(0, 0, ctx.width, ctx.height);
 
 
+      var barWidth = (ctx.width / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+      for(var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i]/2;
+        canvasCtx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+        canvasCtx.fillRect(x,ctx.height-barHeight/2,barWidth,barHeight);
+        x += barWidth + 1;
+      }
+  };
+  draw();
+  /* use the stream */
+}).catch(function(err) {
+  /* handle the error */
+});
 
-
-
-
-
-
-
+// var source = audioCtx.createMediaElementSource(audio);
