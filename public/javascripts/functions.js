9e03c6f8956bd00631cb7c9501f0d50608e7273c
eagerNewb <kk.manoilov@gmail.com>
2017-03-21 09:39:02 +0200

    play, pause; volume control;

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,17 +1,32 @@
 var soundID = "nosecurity";
 createjs.Sound.alternateExtensions = ["mp3"];
-createjs.Sound.on("fileload", this.loadHandler, this);
+// createjs.Sound.on("fileload", this.loadHandler, this);
 createjs.Sound.registerSound("assets/nosecurity.mp3", soundID);
- 
+var instance = createjs.Sound.play(soundID);  //this is the instance object
+var playPauseBtn = document.getElementById('play-btn');
+//pause and play song
+playPauseBtn.addEventListener('click', playPause);
+// volumeControl.addEventListener('change', volumeUpDown(volumeControlVal));
+function playPause(){
+	// console.log("btn has been clicked", event)
+     if(instance.playState == createjs.Sound.PLAY_SUCCEEDED){
+     	 instance.stop()
+     }else{
+     	 instance.play(soundID)
+     }
+}
 
- function loadHandler(event) {
-     // This is fired for each sound that is registered.
-     var instance = createjs.Sound.play(soundID);  // play using id.  Could also use full sourcepath or event.src.
-     instance.on("complete", this.handleComplete, this);
-     instance.volume = 0.5;
+function volumeUpDown(volume){
+    var instanceVolume = volume * 1/100;
+	instance.volume = instanceVolume;	
+	console.log(instanceVolume)	 
+
+}
 
 
- }
+
+
+
 
 
 
